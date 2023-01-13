4e7162f4af917255473a0dc9a11ba38f12fc0cc6
pmbjojo <jourdamj>
2023-01-13 16:00:45 +0000

    maj

--- database/insert/insert_evaluateur.sql
+++ database/insert/insert_evaluateur.sql
@@ -4,3 +4,23 @@
 
 -- Un évaluateur ne peut pas évaluer des dessins de son Club.
 -- Un dessin doit être évalué par deux évaluateurs qui constituent un Jury.
+insert into EVALUATEUR (numEvaluateur, specialite) values (95, 'Comedy');
+insert into EVALUATEUR (numEvaluateur, specialite) values (96, 'Fantasy');
+insert into EVALUATEUR (numEvaluateur, specialite) values (97, 'Drama');
+insert into EVALUATEUR (numEvaluateur, specialite) values (98, 'Horror');
+insert into EVALUATEUR (numEvaluateur, specialite) values (99, 'Crime');
+insert into EVALUATEUR (numEvaluateur, specialite) values (100, 'Comedy');
+insert into EVALUATEUR (numEvaluateur, specialite) values (101, 'Drama');
+insert into EVALUATEUR (numEvaluateur, specialite) values (102, 'Drama');
+insert into EVALUATEUR (numEvaluateur, specialite) values (103, 'Thriller');
+insert into EVALUATEUR (numEvaluateur, specialite) values (104, 'Horror');
+insert into EVALUATEUR (numEvaluateur, specialite) values (105, 'Action');
+insert into EVALUATEUR (numEvaluateur, specialite) values (106, 'Documentary');
+insert into EVALUATEUR (numEvaluateur, specialite) values (107, 'Drama');
+insert into EVALUATEUR (numEvaluateur, specialite) values (108, 'Drama|Horror');
+insert into EVALUATEUR (numEvaluateur, specialite) values (109, 'Comedy');
+insert into EVALUATEUR (numEvaluateur, specialite) values (110, 'Romance');
+insert into EVALUATEUR (numEvaluateur, specialite) values (111, 'Horror');
+insert into EVALUATEUR (numEvaluateur, specialite) values (112, 'Romance');
+insert into EVALUATEUR (numEvaluateur, specialite) values (113, 'Comedy');
+insert into EVALUATEUR (numEvaluateur, specialite) values (114, 'Drama');
