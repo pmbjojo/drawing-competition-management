41fa15f2ebcabe84f956fa7e19f14821edf20d6e
pmbjojo <jourdamj>
2023-01-26 14:45:40 +0000

    commit

--- database/select/RequetePerso1.sql
+++ database/select/RequetePerso1.sql
@@ -2,15 +2,15 @@ WITH best_region AS (SELECT
   CLUB.region,
   CONCOURS.numConcours,
   AVG(EVALUATION.note) as moyenne,
-ROW_NUMBER() OVER (PARTITION BY CONCOURS.numConcours ORDER BY AVG(EVALUATION.note) DESC) as rank
+  ROW_NUMBER() OVER (PARTITION BY CONCOURS.numConcours ORDER BY AVG(EVALUATION.note) DESC) as rank
 FROM 
   DESSIN, EVALUATION, COMPETITEUR, CLUB, PARTICIPE_COMPETITEUR, CONCOURS, UTILISATEUR 
 WHERE DESSIN.numDessin = EVALUATION.numDessin
-AND DESSIN.numCompetiteur = COMPETITEUR.numCompetiteur
-AND COMPETITEUR.numCompetiteur=UTILISATEUR.numUtilisateur
-AND UTILISATEUR.numClub = CLUB.numClub
-AND DESSIN.numCompetiteur = PARTICIPE_COMPETITEUR.numCompetiteur
-AND PARTICIPE_COMPETITEUR.numConcours = CONCOURS.numConcours
+  AND DESSIN.numCompetiteur = COMPETITEUR.numCompetiteur
+  AND COMPETITEUR.numCompetiteur=UTILISATEUR.numUtilisateur
+  AND UTILISATEUR.numClub = CLUB.numClub
+  AND DESSIN.numCompetiteur = PARTICIPE_COMPETITEUR.numCompetiteur
+  AND PARTICIPE_COMPETITEUR.numConcours = CONCOURS.numConcours
 GROUP BY 
   CLUB.region,
   CONCOURS.numConcours)
